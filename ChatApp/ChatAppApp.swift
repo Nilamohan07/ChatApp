41dd89c9d3dc26447c481205d15f4e8f5e6beebc
Udhayanila <nilamohan07@outlook.com>
2025-04-08 21:18:31 +0530

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Udhayanila on 08/04/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
